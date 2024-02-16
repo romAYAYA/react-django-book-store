f7f74d1dc0b02e40ef897fdeccb946cb85857c86
romAYAYA <supernaturalmlady@gmail.com>
2024-02-16 21:55:09 +0600

    ordering added. single book get method added

--- back/django_app/views.py
+++ back/django_app/views.py
@@ -1,6 +1,5 @@
 from django.core.paginator import Paginator
 from rest_framework.decorators import api_view
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -9,13 +8,30 @@ from django_app.models import Book
 
 @api_view(['GET'])
 def get_books(request: Request) -> Response:
-    queryset = Book.objects.all()
+    sort = request.GET.get("sort", "desc")
+
+    books = Book.objects.all()
+
+    match sort:
+        case "name_asc":
+            books = books.order_by("title")
+        case "name_desc":
+            books = books.order_by("-title")
+        case _:
+            books = books.order_by("title")
+
     selected_page = request.GET.get(key="page", default=1)
-    pages = Paginator(object_list=queryset, per_page=2)
+    pages = Paginator(object_list=books, per_page=2)
     page = pages.page(number=selected_page)
 
-    serializer_data = [{"id": x.id, "title": x.title, 'description': x.description} for x in page.object_list]
-    total_count = len(queryset)
-    return Response({"data": serializer_data, "total_count": total_count})
-
+    serializer_books = [{"id": x.id, "title": x.title, 'description': x.description} for x in page.object_list]
     # "book_file": x.book_file
+    total_count = len(books)
+    return Response({"data": serializer_books, "total_count": total_count, "sort": sort})
+
+
+@api_view(['GET'])
+def get_book(request: Request, book_id: str) -> Response:
+    book = Book.objects.get(id=int(book_id))
+    serializer_book = {"id": book.id, "title": book.title, 'description': book.description}
+    return Response({"data": serializer_book})
